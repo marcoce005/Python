cc5b4774b46979b08520d8ab8da88d6593d4301d
marcoce005 <marcoce005@gmail.com>
2024-11-17 19:50:21 +0100

    update es7.2.2

--- poli/esercitazione7/es7.2.2.py
+++ poli/esercitazione7/es7.2.2.py
@@ -1,2 +1,20 @@
+def find_biggest_space(l):
+    return
+
+def add_car(l):
+    find_biggest_space(l)
+
+
 n = int(input("Inserire il numero di posti che ha la fila [intero]:\t"))
 
+l = ["_" for _ in range(n)]
+
+print("Parheggio\n", *l, sep=" ")
+
+park = input("Inserire y/N per parcheggiare o meno una persona:\t") == "y"
+while park:
+    add_car(l)
+
+    print("Parheggio\n", *l, sep=" ")
+
+    park = input("Inserire y/N per parcheggiare o meno una persona:\t") == "y"
