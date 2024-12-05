06cc8cb5ac6441efab8093b7b04a72662cb6a094
marcoce005 <marcoce005@gmail.com>
2024-12-05 15:14:59 +0100

    add es 10.1.5 and 10.1.6

--- poli/esercitazione10/es10.1.6/es10.1.6.py
+++ poli/esercitazione10/es10.1.6/es10.1.6.py
@@ -9,14 +9,73 @@ def create_cyper(k):
     return key_word
 
 
-def crypt():
-    return
+def crypt(f, cy):
+    return [
+        "".join(l)
+        for l in [
+            [
+                (
+                    cy[ord(c.upper()) - 65]
+                    if c.isupper()
+                    else (
+                        cy[ord(c.upper()) - 65].lower()
+                        if 65 <= ord(c.upper()) <= 90
+                        else c
+                    )
+                )
+                for c in line
+            ]
+            for line in f
+        ]
+    ]
 
 
-def decrypt():
-    return
+def decrypt(f, cy):
+    return [
+        "".join(l)
+        for l in [
+            [
+                (
+                    chr(cy.index(c) + 65)
+                    if c.isupper()
+                    else (
+                        chr(cy.index(c.upper()) + 65).lower()
+                        if 65 <= ord(c.upper()) <= 90
+                        else c
+                    )
+                )
+                for c in line
+            ]
+            for line in f
+        ]
+    ]
 
 
 KEY = "FEATHER"
 
 cyper = create_cyper(KEY)
+
+try:
+    mod = input(
+        "Selezionare la modilità:\n[C] per cifrare un file\n[D] per decifrare un file\n-->"
+    ).upper()
+
+    while mod != "C" and mod != "D":
+        print("Modalità inesistene, Riprovare:\n")
+        mod = input(
+            "Selezionare la modilità:\n[C] per cifrare un file\n[D] per decifrare un file\n-->"
+        ).upper()
+
+    file_path = input(
+        f"\nSelezionare il file da {"cifrare" if mod == "C" else "decifrare"} [es. ciro.txt]\n-->"
+    )
+
+    with open(file_path, "r") as in_file:
+        out = crypt(in_file, cyper) if mod == "C" else decrypt(in_file, cyper)
+
+    with open("chyper_text.txt" if mod == "C" else "plain_text.txt", "w") as out_file:
+        for line in out:
+            out_file.write(line)
+
+except Exception as err:
+    print(err)
