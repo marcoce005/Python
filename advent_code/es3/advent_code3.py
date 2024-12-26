8cfffb0bf00fef8189131b79fb85606d72b4a65e
marcoce005 <marcoce005@gmail.com>
2024-12-26 14:19:12 +0100

    end part1 advent-code 3

--- advent_code/es3/advent_code3.py
+++ advent_code/es3/advent_code3.py
@@ -1,17 +1,6 @@
 import re
 
 
-def clean_line(s):
-    correct_characters = [str(i) for i in range(10)] + list("mul(,)")
-    return "".join([c for c in s if c in correct_characters])
-
-
-def read_file(path):
-    with open(path) as file:
-        return [clean_line(line) for line in file]
-    return result
-
-
 def check_correct_args(s):
     if s[0] != "(":
         return False
@@ -32,7 +21,7 @@ def check_correct_args(s):
 
 def extract_args(s):
     return [
-        [int(e) for e in s[i + 4 :].split(")")[0].split(",")]
+        tuple([int(e) for e in s[i + 4 :].split(")")[0].split(",")])
         for i in range(len(s) - 3)
         if s[i : i + 3] == "mul" and check_correct_args(s[i + 3 : i + 12])
     ]
@@ -45,10 +34,7 @@ def mul_args(l):
 def print_result(l):
     tot = 0
     for line in l:
-        # for i, arg in enumerate(line):
-        # print(*arg, sep="*", end=" + " if i != len(line) - 1 else " = ")
         tot += sum(mul_args(line))
-        # print(sum(mul_args(line)))
     return tot
 
 
@@ -56,25 +42,18 @@ def main():
     FILE_PATH = "./test.txt"
 
     try:
-        clean_file = read_file(FILE_PATH)
+        file = open(FILE_PATH, "r").read().split("\n")
 
-        pattern = r"mul\((\d+),(\d+)\)"
-        matches = [[(int(e[0]), int(e[1])) for e in  re.findall(pattern, line)] for line in clean_file]
+        # pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+        # matches = re.findall(pattern, "".join(file))
+        # result = [int(x) * int(y) for x, y in matches]
+        # print(sum(result))
 
-        result = [e[0] * e[1] for line in matches for e in line]
-        
-        print(sum(result))
-        # print(matches)
-
-        arguments = [extract_args(line) for line in clean_file]
-
-        # print(arguments)
-        
-        # print(arguments == matches)
+        arguments = [extract_args(line) for line in file]
 
         add_all = print_result(arguments)
-        
-        print(f"\n\nAdd all = {add_all}")
+
+        print(f"Add all = {add_all}")
 
     except FileNotFoundError:
         print("File not found")
