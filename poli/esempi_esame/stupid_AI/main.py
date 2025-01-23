c2d8ac1486dfae79b8f22c03b89a59994c4fa4bb
marcoce005 <marcoce005@gmail.com>
2025-01-23 13:10:54 +0100

    update in test esame stupid AI

--- poli/esempi_esame/stupid_AI/main.py
+++ poli/esempi_esame/stupid_AI/main.py
@@ -1,10 +1,11 @@
-import string, sys
+from string import punctuation
+from random import randint, choice
 
 
 def read_file_replace_all_punctuation(path):
     with open(path) as file:
         s = file.read()
-        for e in string.punctuation:
+        for e in punctuation:
             if e in s:
                 s = s.replace(e, "")
     return s
@@ -22,8 +23,38 @@ def count_lemmi(text):
                     d[f"{line[i]} {line[i + 1]}"] = text.count(
                         f"{line[i]} {line[i + 1]}"
                     )
-        
+
     return d
+
+
+def filter_by_first_word(d, w):
+    return dict(
+        sorted(
+            {k: v for k, v in d.items() if k.split()[0] == w}.items(),
+            key=lambda x: x[1],
+            reverse=True,
+        )[:5]
+    )
+
+
+def random_word(l):
+    return
+
+
+def generate_text(l, s):
+    lemmi_start_with_word = filter_by_first_word(l, s)
+
+    text = ""
+    for _ in range(randint(5, 50)):
+        if len(lemmi_start_with_word) == 0:
+            s = random_word(l)
+            lemmi_start_with_word = filter_by_first_word(l, s)
+
+        s = choice(list(lemmi_start_with_word.keys())).split()[1]
+        text += s + " "
+        lemmi_start_with_word = filter_by_first_word(l, s)
+
+    return text
 
 
 def main():
@@ -32,7 +63,10 @@ def main():
     text = read_file_replace_all_punctuation(FILE_PATH).lower()
     lemmi = count_lemmi(text)
 
-    print(lemmi)
+    start_word = input("\nInserire la parola di inizio:\t").lower()
+    while start_word != "":
+        print(generate_text(lemmi, start_word) + "\n")
+        start_word = input("\nInserire la parola di inizio:\t").lower()
 
 
 main()
